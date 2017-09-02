d3462856efcf8cfaf33694b8f2d62b59f1fbbe5f
Hirotaka Wagai <private_mail_r0n_v9v@icloud.com>
2017-09-03 02:44:39 +0900

    Add assignment flag to practice model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814161854) do
+ActiveRecord::Schema.define(version: 20170902140201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20170814161854) do
     t.text "goal"
     t.integer "category_id"
     t.integer "position"
+    t.boolean "assignment", default: false, null: false
     t.index ["category_id"], name: "index_practices_on_category_id"
   end
 
